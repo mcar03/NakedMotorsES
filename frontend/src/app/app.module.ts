56555b874167bac64a64619dbf5355c4026aa40a
mcar03 <mcar03.474@gmnail.com>
2025-04-14 17:40:54 +0200

    Initial commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,

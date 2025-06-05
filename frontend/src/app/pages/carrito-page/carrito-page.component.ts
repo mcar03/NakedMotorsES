9ae988d11d4cacfe6604426313507d331916300d
mcar03 <mcar03.474@gmnail.com>
2025-06-05 11:20:50 +0200

    CArrito, sin Api

--- frontend/src/app/pages/carrito-page/carrito-page.component.ts
+++ frontend/src/app/pages/carrito-page/carrito-page.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface ProductoCarrito {
+  nombre: string;
+  precio: number;
+  cantidad: number;
+  imagen: string;
+}
+
+
 @Component({
   selector: 'app-carrito-page',
   templateUrl: './carrito-page.component.html',
@@ -7,4 +15,30 @@ import { Component } from '@angular/core';
 })
 export class CarritoPageComponent {
 
+  productos: ProductoCarrito[] = [
+    {
+      nombre: 'Casco Integral',
+      precio: 120,
+      cantidad: 1,
+      imagen: 'assets/img/img-prueba-local/casco-shark.jpg'
+    },
+    {
+      nombre: 'Guantes Racing',
+      precio: 45,
+      cantidad: 2,
+      imagen: 'assets/img/img-prueba-local/guantes-carbono.jpg'
+    }
+  ];
+
+  get total(): number {
+    return this.productos.reduce((acc, p) => acc + p.precio * p.cantidad, 0);
+  }
+
+  eliminarProducto(index: number) {
+    this.productos.splice(index, 1);
+  }
+
+  vaciarCarrito() {
+    this.productos = [];
+  }
 }
